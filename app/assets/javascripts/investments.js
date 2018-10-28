c228e3bc8a65a80194986e34a1ade4a8b1adce33
yutingcxiang <yutingcxiang@gmail.com>
2018-10-27 23:54:17 -0400

    Render all investments on latest page with ajax

--- app/assets/javascripts/investments.js
+++ app/assets/javascripts/investments.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $('#load_investments').on('click', function(e) {
     e.preventDefault();
     $.get("/investments", function(data) {
-      console.log(data);
+      $('#render_investments').html(data);
     });
   });
 });
