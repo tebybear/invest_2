fdfe853c90641c08bab271616e932c1e5c4220e0
yutingcxiang <yutingcxiang@gmail.com>
2018-10-29 22:37:34 -0400

    Create User and Fund Javascript Model Objects

--- app/assets/javascripts/funds.js
+++ app/assets/javascripts/funds.js
@@ -1,1 +1,9 @@
 // Render funds's show page via Ajax on funds index page.
+
+
+//User Model Object
+class Fund {
+  constructor(attributes) {
+    this.symbol = attributes.symbol;
+  }
+}
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,9 +3,24 @@
 $(function() {
   $("a.view-profile").on("click", function(e) {
     e.preventDefault();
-    var id = $(this).data("id");
-    $.get("/users/" + id + ".json", function(user) {
+    let id = $(this).data("id");
+    $.get("/users/" + id + ".json", function(data) {
+      user = new User(data);
       console.log(user);
     });
   });
 });
+
+
+//User Model Object
+class User {
+  constructor(attributes) {
+    this.username = attributes.username;
+    this.id = attributes.id;
+    this.funds = attributes.funds;
+    this.investments = attributes.investments;
+  }
+  // renderFunds() {
+  //   $("ul.render-funds").append(this.funds);
+  // }
+}
