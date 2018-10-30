dc6e4dfd831d07aa2bf794b96a5f2455b02b2fc8
yutingcxiang <yutingcxiang@gmail.com>
2018-10-30 00:34:42 -0400

    Add Fund object model methods for totals

--- app/assets/javascripts/funds.js
+++ app/assets/javascripts/funds.js
@@ -1,9 +1,37 @@
 // Render funds's show page via Ajax on funds index page.
+$(function(){
+  $("a.view-fund").on("click", function(e) {
+    e.preventDefault();
+    var fundId = $(this).data("id");
+    $.get("/funds/" + fundId + ".json", function(data) {
+      console.log("Hi");
+    });
+  });
+});
 
+// $(function() {
+//   $("a.view-profile").on("click", function(e) {
+//     e.preventDefault();
+//     var id = $(this).data("id");
+//     $(this).hide();
+//     $.get("/users/" + id + ".json", function(data) {
+//       let user = new User(data);
+//       user.showFunds();
+//     });
+//   });
+// });
 
 //User Model Object
 class Fund {
   constructor(attributes) {
     this.symbol = attributes.symbol;
+    this.users = attributes.users;
+    this.investments = attributes.investments;
+  }
+  totalUsers(){
+    return this.users.length;
+  }
+  totalInvestments(){
+    return this.investments.length;
   }
 }
